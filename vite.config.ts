81603c2fdef51f2b7053e56752c48fbaf6147095
rosevilleage <chanhee.frontend@gmail.com>
2025-12-29 23:06:42 +0900

    chore: gitignore 업데이트 및 dev port 3000으로 변경

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    port: 3000,
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
@@ -17,4 +20,4 @@ export default defineConfig({
       "@/shared": path.resolve(__dirname, "./src/shared"),
     },
   },
-})
+});
